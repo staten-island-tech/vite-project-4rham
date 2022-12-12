a32622b885d442245904d586b156a465edaaf6c4
4rham <95187191+4rham@users.noreply.github.com>
2022-12-12 10:14:09 -0500

    geaghaer

---
+++ vite-project/JS/pokemon.js
@@ -0,0 +1,17 @@
+export const pokemon = [
+    {
+        Name: "Koraidon",
+        OG: "Cyclizar",
+        Number: "1007",
+        Type: "Fighting, Dragon",
+        Image: ,
+    },
+    {
+        Name: "Miraidon",
+        OG: "Cyclizar",
+        Number: "1008",
+        Type: "Electric, Dragon",
+        Image: ,
+        
+    },
+]
