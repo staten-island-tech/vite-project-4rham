ccd76e6a71f24ba33204f54c0363e4086124e6a3
4rham <arhambajwa24@gmail.com>
2023-01-02 15:17:49 -0500

    gsrgfgs

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -2,3 +2,13 @@ import '../css/style.css'
 import {spokemon} from "./pokemon"
 console.log(spokemon)
 
+document.querySelector("#tb").addEventListener("click", function (){
+    if(document.body.classList.contains("violet")){
+        document.body.classList.add("scarlet");
+        document.body.classList.remove("violet");
+    }
+    else {
+        document.body.classList.add("violet")
+        document.body.classList.remove("scarlet")
+    }
+})
